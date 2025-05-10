8e1886a677936306490bb867195dcdce212065de
Braian Palacios <braianpalacios699@gmail.com>
2025-05-10 02:55:21 -0300

    Definicion de estilo en About

--- src/components/CardTrajectory.tsx
+++ src/components/CardTrajectory.tsx
@@ -2,10 +2,10 @@ import { TTrajectory } from "../types/types";
 
 export default function TrajectoryCard({ info_trajectory }: Omit <TTrajectory, 'id_trajectory'>) {
     return (
-        <div className="md:w-1/2">
+        <div className="md:w-10/12 p-1.5">
             <h3 className="text-2xl font-semibold mb-4">Mi trayectoria</h3>
             {info_trajectory.split('/').map((paragraph, index) => (
-                <p key={index} className="text-gray-600 mb-6">
+                <p key={index} className="text-gray-600 mb-6 w-full">
                     {paragraph.trim()}
                 </p>
             ))}
